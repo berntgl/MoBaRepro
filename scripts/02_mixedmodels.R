cdb6519ea4f99559a8f7c8677d382d9118ce1e93
berntgl <bernt@glsr.nl>
2025-04-11 12:54:30 +0200

    Added LRT

--- scripts/02_mixedmodels.R
+++ scripts/02_mixedmodels.R
@@ -9,24 +9,42 @@ library(broom)
 load("./data/longdata.RData")
 
 #   Main analysis
-#   Defining model
-mod <- lmerTest::lmer(height ~ age + breastfeed_dur_factor + sex_num + birth_length + age*sex_num + breastfeed_dur_factor*sex_num+ (1 | id) + (1 | fam_id), data = long_data)
-summary(mod)
+#   Defining baseline model to facilitate LRT of breastfeeding
+mod1 <- lmerTest::lmer(height ~ age + sex_num + birth_length + age*sex_num + (1 | id) + (1 | fam_id), data = long_data)
+summary(mod1)
 
+#   Defining model with breastfeeding duration variable
+mod2 <- lmerTest::lmer(height ~ age + breastfeed_dur_factor + sex_num + birth_length + age*sex_num + breastfeed_dur_factor*sex_num+ (1 | id) + (1 | fam_id), data = long_data)
+summary(mod2)
+
+#   Comparing baseline and breastfeeding models
+lrt_main <- anova(mod1, mod2, test = "LRT")
 
 #   Sensitivity 1: different binning of the breastfeeding variable
 
-#   Defining model
-mod_s1 <- lmerTest::lmer(height ~ age + breastfeed_dur_factor_s1 + sex_num + birth_length + age*sex_num + breastfeed_dur_factor_s1*sex_num+ (1 | id) + (1 | fam_id), data = long_data)
-summary(mod_s1)
+#   Defining baseline model
+mod1_s1 <- lmerTest::lmer(height ~ age + sex_num + birth_length + age*sex_num + (1 | id) + (1 | fam_id), data = long_data)
+summary(mod1_s1)
 
+#   Defining model with alternative breastfeeding duration variable
+mod2_s1 <- lmerTest::lmer(height ~ age + breastfeed_dur_factor_s1 + sex_num + birth_length + age*sex_num + breastfeed_dur_factor_s1*sex_num+ (1 | id) + (1 | fam_id), data = long_data)
+summary(mod2_s1)
+
+#   Comparing baseline and breastfeeding duration models
+lrt_s1 <- anova(mod1_s1, mod2_s1, test = "LRT")
 
 #   Sensitivity 2: Different threshold criteria for outliers in outcome variable
 
-#   Defining model
-mod_s2 <- lmerTest::lmer(height_s2 ~ age + breastfeed_dur_factor + sex_num + birth_length + age*sex_num + breastfeed_dur_factor*sex_num+ (1 | id) + (1 | fam_id), data = long_data)
-summary(mod_s2)
+#   Defining baseline model
+mod1_s2 <- lmerTest::lmer(height_s2 ~ age + sex_num + birth_length + age*sex_num + (1 | id) + (1 | fam_id), data = long_data)
+summary(mod1_s2)
 
+#   Defining model with breastfeeding duration variable
+mod2_s2 <- lmerTest::lmer(height_s2 ~ age + breastfeed_dur_factor + sex_num + birth_length + age*sex_num + breastfeed_dur_factor*sex_num+ (1 | id) + (1 | fam_id), data = long_data)
+summary(mod2_s2)
+
+#   Comparing baseline and breastfeeding duration models. 
+lrt_s2 <- anova(mod1_s2, mod2_s2, test = "LRT")
 
 #   Collating results
 
@@ -34,15 +52,24 @@ summary(mod_s2)
 source("./scripts/helper_function.R")
 
 # Extract summaries from each model
-results_main <- extract_summary(mod, "Main")
-results_s1 <- extract_summary(mod_s1, "Sensitivity 1")
-results_s2 <- extract_summary(mod_s2, "Sensitivity 2")
+results_main1 <- extract_summary(mod1, "Main", "Baseline")
+results_main2 <- extract_summary(mod2, "Main", "Breastfeeding")
+results_s1_1 <- extract_summary(mod1_s1, "S1", "Baseline")
+results_s1_2 <- extract_summary(mod2_s1, "S1", "Breastfeeding")
+results_s2_1 <- extract_summary(mod1_s2, "S2", "Baseline")
+results_s2_2 <- extract_summary(mod2_s2, "S2", "Breastfeeding")
 
 # Combine results into a single table
-combined_results <- bind_rows(results_main, results_s1, results_s2) %>%
-  dplyr::select(analysis, term, Estimate = "Estimate", Std.Error = "Std. Error", t.value = "t value", p.value = "p.value")
+combined_results <- bind_rows(results_main1, results_main2, results_s1_1, results_s1_2, results_s2_1, results_s2_2) %>%
+  dplyr::select(analysis, model, term, Estimate = "Estimate", Std.Error = "Std. Error", t.value = "t value", p.value = "p.value")
 
 save(combined_results, file="./data/res.RData")
 
+# Combine LRT results into a single table
+combined_results_lrt <- bind_rows(lrt_main, lrt_s1, lrt_s2)
+
+save(combined_results_lrt, file="./data/res_lrt.RData")
+
 # Print combined table
 print(combined_results)
+print(combined_results_lrt)
--- scripts/helper_function.R
+++ scripts/helper_function.R
@@ -1,15 +1,13 @@
 library(tidyverse)
 
 # Define a function to extract summary coefficients and convert to a dataframe
-extract_summary <- function(model, analysis_label) {
+extract_summary <- function(model, analysis_label, model_label) {
   coef_summary <- summary(model)$coefficients
   coef_summary <- as.data.frame(coef_summary) %>%
     rownames_to_column("term") %>%
     mutate(analysis = analysis_label,
-           p.value = `Pr(>|t|)`,
-           p.value = ifelse(grepl("breastfeed_dur_factor", term),
-                            p.adjust(p.value, method = "bonferroni"),
-                            p.value)) %>%
+           model = model_label,
+           p.value = `Pr(>|t|)`) %>%
     dplyr::select(-`Pr(>|t|)`)
   
   
